d3ec823e33b67f589039706acee50748b0d0b4a4
nastya-khaletskaya <nastyahaletsksya@gmail.com>
2022-05-28 09:17:43 +0300

    add api

--- src/component/books/Books.tsx
+++ src/component/books/Books.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import BooksCard from './book/card/BooksCard';
 import data from './data';
 import BookType from '../../types/bookType';
@@ -8,22 +8,22 @@ import './Books.scss';
 
 
 
-
-
-
-type PropsType = {}
+type PropsType = {};
 
 const URL = "https://api.itbook.store/1.0/";
 
 const Books: React.FC<PropsType> = () => {
-  // useEffect(() => {
-  //   fetch(URL)
-  //     .then((response) => (response.json))
-  //     .then((data) => {
-  //       const books = data.results as BookType[];
-  //       console.log(books);
-  //     })
-  // })
+
+  const [books, setBooks] = useState<BookType[]>([]);
+  useEffect(() => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => {
+        const books = data.results as BookType[];
+        setBooks(books);
+        console.log(books);
+      })
+  }, []);
 
   return (
     <div className="books-container">
