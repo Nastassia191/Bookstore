43ac34319a16a022d39591ada4056267e3131ef6
nastya-khaletskaya <nastyahaletsksya@gmail.com>
2022-06-13 09:37:11 +0300

    move translate

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import Timer from '../timer/Timer';
 import { ReactComponent as Logo } from "../assets/logo.svg";
 import { Link } from "react-router-dom";
+import ThemeControl from "./theme/ThemeIcon";
+import useTranslete from "../hooks/useTranslete";
 
 
 import "./Header.scss";
-import ThemeControl from "./theme/ThemeIcon";
 
 
 
@@ -13,9 +14,7 @@ import ThemeControl from "./theme/ThemeIcon";
 
 const Header: React.FC = () => {
 
-
-
-
+  const { lang, setLang } = useTranslete();
 
   return (
     <nav className="header-container">
@@ -40,6 +39,18 @@ const Header: React.FC = () => {
           <Link to="/registration">
             Registration
           </Link>
+        </li>
+        <li>
+          {lang === "en"
+            ?
+            <button className='button_registration_lang' onClick={() => setLang("ru")}>
+              ru
+            </button>
+            :
+            <button className='button_registration_lang' onClick={() => setLang("en")}>
+              en
+            </button>
+          }
         </li>
         <li>
           <ThemeControl />
